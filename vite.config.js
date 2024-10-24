dde0e42372573af203190ebc60f0f0d26fabe675
Md Rasel khan <raselkhanlab@gmail.com>
2024-10-24 15:03:23 +0600

    chore: add path alias and update vite config to support path alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      'views': path.resolve(__dirname, 'src/views')
+    }
+  },
   plugins: [react()],
 })
