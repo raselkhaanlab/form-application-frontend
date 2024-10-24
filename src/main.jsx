3f13af730420cbafb61c43cfd9f463cae6ea921e
Md Rasel khan <raselkhanlab@gmail.com>
2024-10-24 20:15:28 +0600

    feat: add components, views, redux store, firebase auth and firestore

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from "./App"
+import React from "react";
+import {createRoot} from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "store/index";
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>
+);
+
--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,13 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 export default defineConfig({
   resolve: {
     alias: {
+      'assets': path.resolve(__dirname, 'src/assets'),
+      'config': path.resolve(__dirname, 'src/config'),
       'views': path.resolve(__dirname, 'src/views'),
       'data': path.resolve(__dirname, 'src/data'),
       'components': path.resolve(__dirname, 'src/components'),
+      'services': path.resolve(__dirname, 'src/services'),
+      'store': path.resolve(__dirname, 'src/store'),
 
     }
   },
